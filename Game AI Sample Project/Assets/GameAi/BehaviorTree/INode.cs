22521b6941cee5e9eb407acb261a2e2d1f2e6c2f
kraemer-raimund <kraemer_raimund@web.de>
2019-07-13 16:47:33 +0200

    Implement behavior tree and standard nodes

---
+++ Game AI Sample Project/Assets/GameAi/BehaviorTree/INode.cs
@@ -0,0 +1,17 @@
+﻿/*
+* MIT License
+*
+* Copyright(c) 2019 Raimund Krämer (kraemer-raimund)
+*
+* For the full copyright and license information, please refer to the LICENSE file
+* that was distributed with this source code.
+*/
+
+
+namespace RaKrae.GameAi.BehaviorTree
+{
+    public interface INode
+    {
+        NodeResult Execute(IContext context);
+    }
+}
